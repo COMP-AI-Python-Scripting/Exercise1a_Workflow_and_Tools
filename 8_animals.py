fc24e0eb03a56f24d244d53ff430911fe0c03ef0
Martin Robinson <martin.robinson@atu.ie>
2025-10-02 20:27:04 +0100

    Initial Commit of the Solutions
    Solutions that were used to generate code output in the exercise sheet.

---
+++ 8_animals.py
@@ -0,0 +1,6 @@
+animals = ['llama', 'crocodile', 'poodle', 'tiger']
+
+for animal in animals:
+    print(f'A {animal} would make a great pet!')
+
+print('Any of these animals would make great pets!')
